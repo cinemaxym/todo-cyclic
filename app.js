07ed369a9aa01470f9367b6edc47785bb185612d
Max Tukanov <maxim.tukanov@gmail.com>
2023-06-09 10:59:34 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose'); // Import the Mongoose library
+const dotenv = require('dotenv').config();
+
+
+const usersRouter = require('./routes/users'); //Import usersRouter
+const todosRouter = require('./routes/todos') //Import todosRouter
+
+const app = express(); // Create a new express application
+
+let PORT = process.env.PORT || 8080 ; // Set the server's port to either the environment variable PORT or the port number 8080
+
+app.use(express.json()); // Use built-in express middleware to parse JSON data in request bodies
+
+app.use('/', todosRouter); // Mount the todosRouter middleware on the root URL path
+app.use('/', usersRouter); // Mount the usersRouter middleware on the root URL path
+
+// Connect to the MongoDB database using the provided connection string
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to MongoDB')) // Log a success message if the connection is successful
+.catch((error) => // Log an error message if the connection fails
+console.log('Error connecting to MongoDB:', error.message));
+
+// Serve the static files from the frontend build folder
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+// Start the server and listen for incoming requests on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is listening on Port: ${PORT}`);
+});
+
+module.exports = app;
